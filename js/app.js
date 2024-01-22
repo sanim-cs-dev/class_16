b5b4d2326c6cf19c8d94c2ed5e50c14f948f1adb
sanim-cs-dev <sanim.cs.dev@gmail.com>
2024-01-22 12:59:44 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,65 @@
+
+
+// odd and even number 
+
+
+// let num = Number(prompt('Enter number'));
+
+// if (num % 2 === 0){
+//     document.write('Even Number')
+// }
+// else {
+//     document.write('Odd Number')
+// }
+
+
+
+
+
+
+
+
+
+// result marks
+
+let num = Number(prompt('Enter your marks'));
+
+if (num >= 80 && num <= 100 ){
+    document.write('A+')
+}
+else if (num >= 70 && num < 80 ){
+    document.write('A')
+}
+else if (num >= 60 && num < 70 ){
+    document.write('A-')
+}
+else if (num >= 50 && num < 60 ){
+    document.write('B')
+}
+else if (num >= 40 && num < 50 ){
+    document.write('C')
+}
+else if (num >= 33 && num < 40 ){
+    document.write('D')
+}
+else if (num >= 0 && num < 33 ){
+    document.write('F')
+}
+
+
+else {
+    document.write('Invalid Marks')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
